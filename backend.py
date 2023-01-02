5605ac24c3cc67da943551df35bbc2f7be544463
gsr142 <76536836+gsr142@users.noreply.github.com>
2023-01-02 13:04:22 -0800

    added images for sky condition, added Key error exception

--- backend.py
+++ backend.py
@@ -1,5 +1,16 @@
+import requests
+
 API_KEY = "a31b31ba27b38ec38198e6b6a34bb952"
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
-    return data
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+
+    return filtered_data
+
+if __name__=="__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
